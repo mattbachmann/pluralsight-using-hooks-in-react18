0d178e30fb10462cbea571e452443eb8d39b4805
matthias.bachmann <matthias.bachmann@ecofin.ch>
2024-09-19 07:24:28 +0200

    useEffect

--- m3/m3-clip-02/pages/demo.js
+++ m3/m3-clip-02/pages/demo.js
@@ -1,8 +1,25 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 
 export default function Demo() {
   const [text1, setText1] = useState("First");
   const [text2, setText2] = useState("Last");
+
+  useEffect(() => {
+      console.log('mount?');
+
+      return () => {
+          console.log('unmount?');
+      }
+  }, []); // Only mount/unmount handler if empty deps array
+
+  useEffect(() => {
+      console.log('text1', text1);
+      console.log('text2', text2);
+
+      return () => {
+        console.log('cleanup called after every change');
+      };
+  }, [text1, text2]);
 
   return (
     <div className="container">
